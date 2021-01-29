8c4905f1cb5bac656de377e889baf59129f4ce69
Fear <dolgih2@olimp.lan>
2021-01-29 09:52:53 +0300

    delay property can attach to produser
    apply new handler now need handle

--- tests/async/delayed-property-test.cpp
+++ tests/async/delayed-property-test.cpp
@@ -1,13 +1,19 @@
+#include <atomic>
 #include <stdexcept>
 #include <thread>
 #include <gtest/gtest.h>
 
 #include <nhope/async/ao-context.h>
 #include <nhope/async/delayed-property.h>
+#include <nhope/seq/func-produser.h>
 
 namespace {
 
 constexpr int testValue = 10;
+constexpr auto maxProduseCount{100};
+
+constexpr auto nullHandler = [](auto /*unused*/) {
+};
 
 using namespace nhope;
 using namespace std::literals;
@@ -37,7 +43,7 @@ TEST(DelayedProperty, exception)   // NOLINT
     EXPECT_TRUE(prop.hasNewValue());
     auto f2 = prop.setNewValue(1);
     EXPECT_THROW(f.get(), nhope::AsyncOperationWasCancelled);   //NOLINT
-    prop.applyNewValue();
+    prop.applyNewValue(nullHandler);
     f2.get();
     EXPECT_EQ(prop.getCurrentValue(), 1);
 }
@@ -47,7 +53,7 @@ TEST(DelayedProperty, waitPropertyTimeout)   // NOLINT
     DelayedProperty prop(0);
     auto t = std::thread([&] {
         EXPECT_TRUE(prop.waitNewValue(20ms));
-        prop.applyNewValue();
+        prop.applyNewValue(nullHandler);
         EXPECT_EQ(prop.getCurrentValue(), testValue);
     });
     std::this_thread::sleep_for(10ms);
@@ -62,7 +68,7 @@ TEST(DelayedProperty, waitPropertyTimeout)   // NOLINT
     }).detach();
 
     prop.waitNewValue();
-    prop.applyNewValue();
+    prop.applyNewValue(nullHandler);
 
     EXPECT_EQ(prop.getCurrentValue(), 1);
 }
@@ -75,4 +81,58 @@ TEST(DelayedProperty, setEqualValue)   // NOLINT
     EXPECT_FALSE(prop.hasNewValue());
     f.get();
     EXPECT_EQ(prop.getCurrentValue(), testValue);
+}
+
+TEST(DelayedProperty, producer)   // NOLINT
+{
+    DelayedProperty prop(testValue);
+
+    FuncProduser<int> numProduser([m = 0](int& value) mutable -> bool {
+        if (m == maxProduseCount + 1) {
+            return false;
+        }
+
+        value = m++;
+        return true;
+    });
+
+    prop.attachToProduser(numProduser);
+
+    numProduser.start();
+    numProduser.wait();
+    prop.applyNewValue(nullHandler);
+    EXPECT_EQ(prop.getCurrentValue(), maxProduseCount);
+}
+
+TEST(DelayedProperty, destroyBeforeProducerEnd)   // NOLINT
+{
+    std::atomic_bool close{false};
+
+    FuncProduser<int> numProduser([m = 0, &close](int& value) mutable -> bool {
+        if (close.load()) {
+            return false;
+        }
+
+        value = m++;
+        return true;
+    });
+    numProduser.start();
+
+    {
+        DelayedProperty prop(testValue);
+        prop.attachToProduser(numProduser);
+        prop.attachToProduser(numProduser);
+        prop.attachToProduser(numProduser);
+        std::this_thread::sleep_for(2ms);
+
+        int value = 0;
+        prop.applyNewValue([&value](auto val) {
+            value = val;
+        });
+        EXPECT_EQ(prop.getCurrentValue(), value);
+    }
+    std::this_thread::sleep_for(20ms);
+    close.store(true);
+
+    numProduser.wait();
 }
