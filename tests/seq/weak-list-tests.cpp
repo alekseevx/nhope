c61315f215038094f96f31fcc642a9a0028d7e65
Fear <dolgih2@olimp.lan>
2021-02-18 14:29:17 +0300

    add test for weak list

--- tests/seq/weak-list-tests.cpp
+++ tests/seq/weak-list-tests.cpp
@@ -2,6 +2,7 @@
 
 #include <memory>
 #include <nhope/seq/weak_list.h>
+#include <vector>
 
 namespace {
 using namespace nhope;
@@ -26,6 +27,40 @@ TEST(WeakList, simpleList)   //NOLINT
     ASSERT_EQ(size, weak.size());
 
     weak.clearExpired();
+
+    ASSERT_TRUE(weak.empty());
+}
+
+TEST(WeakList, sharedList)   //NOLINT
+{
+    static constexpr int size = 1000;
+
+    WeakList<int> weak;
+    EXPECT_TRUE(weak.empty());
+
+    std::vector<std::shared_ptr<int>> temp;
+    temp.reserve(size);
+
+    for (size_t i = 0; i < size; i++) {
+        auto t = std::make_shared<int>(i);
+        temp.emplace_back(t);
+        weak.emplace_back(t);
+    }
+
+    int counter = 0;
+    for (auto x : weak) {
+        counter++;
+    }
+
+    ASSERT_EQ(size, weak.size());
+    temp.clear();
+    weak.clearExpired();
+
+    EXPECT_EQ(counter, size);
+
+    for (auto x : weak) {
+        FAIL() << "list expired ";
+    }
 
     ASSERT_TRUE(weak.empty());
 }
