cb1b4ab2dce46210b1773278aef3a99e7e9aadcb
alekseev <alekseev@olimp.lan>
2021-06-08 09:06:18 +0300

    nhope/utils: Checked template parameter in ScopeExit

--- include/nhope/utils/scope-exit.h
+++ include/nhope/utils/scope-exit.h
@@ -2,14 +2,19 @@
 
 #include <exception>
 
+#include "noncopyable.h"
+#include "type.h"
+
 namespace nhope {
 
 template<typename Fn>
-class ScopeExit
+class ScopeExit : public Noncopyable
 {
     Fn m_fn;
 
 public:
+    static_assert(checkFunctionSignatureV<Fn, void>, "expect void() signature");
+
     explicit ScopeExit(Fn&& f)
       : m_fn(std::forward<Fn>(f))
     {}
