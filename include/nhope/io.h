c1ec80b3f34be9ea4dffcd8779c08f3a2593329e
alekseev <alekseev@olimp.lan>
2021-10-19 16:35:02 +0300

    io: Update io.h

--- include/nhope/io.h
+++ include/nhope/io.h
@@ -1,7 +1,10 @@
 #pragma once
 
+#include "nhope/io/bit-seq-reader.h"
 #include "nhope/io/file.h"
 #include "nhope/io/io-device.h"
+#include "nhope/io/network.h"
+#include "nhope/io/null-device.h"
 #include "nhope/io/serial-port.h"
 #include "nhope/io/string-reader.h"
 #include "nhope/io/tcp.h"
