72e36804c2296137597bb02ddf66020f42dd48f5
alekseev <alekseev@olimp.lan>
2021-11-11 17:29:32 +0300

    io: Update io.h

--- include/nhope/io.h
+++ include/nhope/io.h
@@ -5,6 +5,8 @@
 #include "nhope/io/io-device.h"
 #include "nhope/io/network.h"
 #include "nhope/io/null-device.h"
+#include "nhope/io/pushback-reader.h"
 #include "nhope/io/serial-port.h"
 #include "nhope/io/string-reader.h"
+#include "nhope/io/string-writter.h"
 #include "nhope/io/tcp.h"
